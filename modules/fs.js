5035930fc2cc7ea72ecea89b678f37785fbdc356
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2025-02-01 02:58:03 +0500

    add events method

--- modules/fs.js
+++ modules/fs.js
@@ -1,39 +1,39 @@
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
-// mkdir
-// fs.mkdir(path.join(__dirname, "templates"), (err) => {
+// // mkdir
+// // fs.mkdir(path.join(__dirname, "templates"), (err) => {
+// //   if (err) throw new Error();
+
+// //   console.log("Folder was created successfully");
+// // });
+
+// // fs.mkdir(path.join(__dirname, "notes"), (err) => {
+// //   if (err) throw new Error();
+// // });
+
+// // writeFile
+// fs.writeFile(
+//   path.join(__dirname, "notes", "february.txt"),
+//   "Create new course NodeJS",
+//   (err) => {
+//     if (err) throw new Error();
+//   }
+// );
+
+// // appendFile
+// fs.appendFile(
+//   path.join(__dirname, "notes", "february.txt"),
+//   " and microservice project",
+//   (err) => {
+//     console.log(err);
+//     if (err) throw new Error();
+//     console.log(`Success`);
+//   }
+// );
+
+// // readFIle
+// fs.readFile(path.join(__dirname, "notes", "february.txt"), (err, data) => {
 //   if (err) throw new Error();
-
-//   console.log("Folder was created successfully");
+//   console.log(Buffer.from(data).toString());
 // });
-
-// fs.mkdir(path.join(__dirname, "notes"), (err) => {
-//   if (err) throw new Error();
-// });
-
-// writeFile
-fs.writeFile(
-  path.join(__dirname, "notes", "february.txt"),
-  "Create new course NodeJS",
-  (err) => {
-    if (err) throw new Error();
-  }
-);
-
-// appendFile
-fs.appendFile(
-  path.join(__dirname, "notes", "february.txt"),
-  " and microservice project",
-  (err) => {
-    console.log(err);
-    if (err) throw new Error();
-    console.log(`Success`);
-  }
-);
-
-// readFIle
-fs.readFile(path.join(__dirname, "notes", "february.txt"), (err, data) => {
-  if (err) throw new Error();
-  console.log(Buffer.from(data).toString());
-});
