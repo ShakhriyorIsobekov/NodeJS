85bd946df5a08013c3d0202fd02ac7c1f408a76a
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2025-02-01 23:17:02 +0500

    add working with HTML

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const http = require("http");
-const server = http.createServer((request, response) => {
-  // request = serverga so'rov
-  // response = javob
-  console.log(request.url);
-  response.write("<h1>Hello Shakhriyor Hafizul Qur'an</h1> ");
-  response.end();
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  if (req.method == "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    if (req.url == "/") {
+      fs.readFile(
+        path.join(__dirname, "templates", "index.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw new Error();
+          res.end(content);
+        }
+      );
+    } else if (req.url == "/about") {
+      fs.readFile(
+        path.join(__dirname, "templates", "about.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw new Error();
+          res.end(content);
+        }
+      );
+    } else if (req.url == "/contact") {
+      fs.readFile(
+        path.join(__dirname, "templates", "contact.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw new Error();
+          res.end(content);
+        }
+      );
+    }
+  } else if (req.method == "POST") {
+    const names = [];
+    req.on("data", (data) => {
+      names.push(Buffer.from(data));
+    });
+    req.on("end", () => {
+      const msg = names.toString().split("=")[1];
+      res.end(`Your name is: ${msg}`);
+    });
+  }
 });
 
 server.listen(3000, () => {
