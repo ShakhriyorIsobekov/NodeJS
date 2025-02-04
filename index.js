a32d8f201c7835afc6a359666f75dc9ecb4ca093
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2025-02-04 23:01:45 +0500

    add middleware

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { create, engine } from "express-handlebars";
+import AuthRoutes from "./routes/auth.js";
+import ProductRoutes from "./routes/products.js";
 
 const app = express();
 const hbs = create({ defaultLayout: "main", extname: "hbs" });
@@ -8,12 +10,11 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
+app.use(AuthRoutes);
+app.use(ProductRoutes);
+
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
 });
 
 const PORT = process.env.PORT || 4100;
