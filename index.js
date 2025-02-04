d819052a5565099fcca6918f220cef19927f01f5
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2025-02-04 21:40:48 +0500

    add layout of handlebars

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import express from "express";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import { engine } from "express-handlebars";
+import { create, engine } from "express-handlebars";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const app = express();
+const hbs = create({ defaultLayout: "main", extname: "hbs" });
 
-app.engine("hbs", engine());
+app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
